0f62e08956e401e1028ebf5dc401dc6f9e32eb8a
p-ras-run <65076909+p-ras-run@users.noreply.github.com>
2020-05-17 20:43:30 +0800

    Create Jobsheet 1 C.cpp
    Menampilkan array satu dimensi

---
+++ Jobsheet 1 C.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+main (){
+int nim[14]= {2, 1, 9, 4, 7, 8, 12, 13};
+
+printf ("Nim ke-1 = %i\n",nim[0]);
+printf ("Nim ke-2 = %i\n",nim[1]);
+printf ("Nim ke-3 = %i\n",nim[2]);
+printf ("Nim ke-4 = %i\n",nim[3]);
+printf ("Nim ke-5 = %i\n",nim[4]);
+printf ("Nim ke-6 = %i\n",nim[5]);
+printf ("Nim ke-7 = %i\n",nim[6]);
+printf ("Nim ke-8 = %i\n",nim[7]);
+printf ("Nim ke-9 = %i\n",nim[5]);
+printf ("Nim ke-10 = %i\n",nim[5]);
+printf ("Nim ke-11 = %i\n",nim[5]);
+printf ("Nim ke-12 = %i\n",nim[5]);
+printf ("Nim ke-13 = %i\n",nim[5]);
+printf ("Nim ke-14 = %i\n",nim[5]);
+}
