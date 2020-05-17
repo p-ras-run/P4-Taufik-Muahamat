9cc1896f6daf8b046572614a4fb7aefe1db64e97
p-ras-run <65076909+p-ras-run@users.noreply.github.com>
2020-05-17 20:44:56 +0800

    Create Jobsheet 1 C++.cpp
    Menampilkan array satu dimensi

---
+++ Jobsheet 1 C++.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Prasetyo Asnawy Pangestu (F1B019114)\n";
+int nim[14]= {15,16,17,18,19,20,21,22,23,24,25,26,27,28};
+	cout << "Nim ke-1 adalah " << nim[0] << endl;
+	cout << "Nim ke-2 adalah " << nim[1] << endl;
+	cout << "Nim ke-3 adalah " << nim[2] << endl;
+	cout << "Nim ke-4 adalah " << nim[3] << endl;
+	cout << "Nim ke-5 adalah " << nim[4] << endl;
+	cout << "Nim ke-6 adalah " << nim[5] << endl;
+	cout << "Nim ke-7 adalah " << nim[6] << endl;
+	cout << "Nim ke-8 adalah " << nim[7] << endl;
+	cout << "Nim ke-9 adalah " << nim[8] << endl;
+	cout << "Nim ke-10 adalah " << nim[9] << endl;
+	cout << "Nim ke-11 adalah " << nim[10] << endl;
+	cout << "Nim ke-12 adalah " << nim[11] << endl;
+	cout << "Nim ke-13 adalah " << nim[12] << endl;
+	cout << "Nim ke-14 adalah " << nim[13] << endl;
+}
